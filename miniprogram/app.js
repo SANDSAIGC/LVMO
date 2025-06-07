a18a3f4b27b3578cddd6a81ed7f5f4d0a92b2a25
SANDSAIGC <274952041@qq.com>
2025-06-08 01:34:05 +0800

    📱 添加小程序核心配置文件
    - app.js: 小程序入口文件，初始化云开发
    - app.json: 页面路由和tabBar配置
    - app.wxss: Tesla风格全局样式
    - sitemap.json: 搜索优化配置

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,36 +1,31 @@
 // app.js
 App({
-  onLaunch: function() {
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+
     // 初始化云开发
     if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
-    wx.cloud.init({
-      env: 'sands-0guvvxuy305fdeff', // 云开发环境ID
-      traceUser: true
-    });
+      wx.cloud.init({
+        env: 'sands-0guvvxuy305fdeff',
+        traceUser: true,
+      })
     }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: res => {
-              this.globalData.userInfo = res.userInfo;
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res);
-              }
-            }
-          });
-        }
-      }
-    });
   },
-  
   globalData: {
+    userInfo: null,
+    deviceConnected: false,
+    currentSession: null
   }
-});
+})
